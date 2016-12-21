ce6b5956e0720c021d5fd8e31528967bd408154c
Rabasco0 <rabasquinho1>
2016-12-21 23:42:47 +0000

    layouts

---
+++ app/src/main/java/pmd/di/ubi/pt/titcherspet/LoginRequest.java
@@ -0,0 +1,29 @@
+package pmd.di.ubi.pt.titcherspet;
+
+/**
+ * Created by Rabasco0 on 21/12/2016.
+ */
+
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "https://calfless-animal.000webhostapp.com/login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String Email, String Password, Response.Listener<String> listener) {
+        super(Method.POST, LOGIN_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", Email);
+        params.put("password", Password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
