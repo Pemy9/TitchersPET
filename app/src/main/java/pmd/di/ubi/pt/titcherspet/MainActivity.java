89c0a08f1cd51ff5b851f1a557289b4d73c1bc6f
Pemy9 <pmms.pemy9@gmail.com>
2016-12-21 17:11:35 +0000

    Layout login e splash screen feito.

--- app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
+++ app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
@@ -1,13 +1,24 @@
 package pmd.di.ubi.pt.titcherspet;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView oTView1;
+    ImageButton oButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Login);
+        setContentView(R.layout.login);
+
+        oTView1 = (TextView)findViewById(R.id.app_name1);
+        oButton1 = (ImageButton) findViewById(R.id.loginButton);
+
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/BlackBoard.ttf");
+        oTView1.setTypeface(face);
     }
 }
