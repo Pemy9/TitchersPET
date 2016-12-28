b95d08763ebe27ef954861c249657567d15c30f2
Pemy9 <pmms.pemy9@gmail.com>
2016-12-28 18:09:13 +0000

    Login

--- app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
+++ app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView oTView1;
     ImageButton oButton1;
+    EditText UsernameEt, PasswordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         oTView1 = (TextView)findViewById(R.id.app_name1);
         oButton1 = (ImageButton) findViewById(R.id.loginButton);
+        UsernameEt = (EditText) findViewById(R.id.username) ;
+        PasswordEt = (EditText) findViewById(R.id.password);
 
         Typeface face= Typeface.createFromAsset(getAssets(), "fonts/BlackBoard.ttf");
         oTView1.setTypeface(face);
     }
 
     public void onLogin(View v){
-        Intent intent1 = new Intent(this, AdminActivity.class);
-        startActivity(intent1);
+
+        if(TextUtils.isEmpty(UsernameEt.getText().toString())){
+            UsernameEt.setError("Não inseriu o username!");
+            return;
+        }
+        String username = UsernameEt.getText().toString();
+
+        if(TextUtils.isEmpty(PasswordEt.getText().toString())){
+            PasswordEt.setError("Não inseriu a password!");
+            return;
+        }
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
     }
 
-    public void onTeste(View v){
-        Intent intent5 = new Intent(this, EducadoraActivity.class);
-        startActivity(intent5);
-    }
 }
