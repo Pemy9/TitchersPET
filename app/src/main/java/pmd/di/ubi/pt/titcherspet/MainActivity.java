1691a1774972e323acc960c14f7750bda1471071
Pemy9 <pmms.pemy9@gmail.com>
2017-01-03 22:07:13 +0000

    XMLs corrigidos e alterada a font do título da app na Main Activity.

--- app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
+++ app/src/main/java/pmd/di/ubi/pt/titcherspet/MainActivity.java
@@ -1,11 +1,13 @@
 package pmd.di.ubi.pt.titcherspet;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.kosalgeek.asynctask.AsyncResponse;
 import com.kosalgeek.asynctask.PostResponseAsyncTask;
@@ -14,6 +16,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse, View.OnClickListener {
 
+    TextView tv1;
     ImageButton oButton1;
     EditText UsernameEt, PasswordEt;
 
@@ -22,11 +25,15 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse, Vi
             super.onCreate(savedInstanceState);
             setContentView(R.layout.login);
 
+            tv1 = (TextView) findViewById(R.id.app_name1);
             oButton1 = (ImageButton) findViewById(R.id.loginButton);
             UsernameEt = (EditText) findViewById(R.id.username) ;
             PasswordEt = (EditText) findViewById(R.id.password);
 
             oButton1.setOnClickListener(this);
+
+        Typeface face= Typeface.createFromAsset(getAssets(), "fonts/BlackBoard.ttf");
+        tv1.setTypeface(face);
 
     }
 
